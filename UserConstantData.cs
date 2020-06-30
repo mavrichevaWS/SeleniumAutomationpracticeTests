3c4e945da4f094316a5fa1803c98681b05bdeea1
Lena Mavricheva <59619343+mavrichevaWS@users.noreply.github.com>
2020-06-30 08:57:36 +0300

    Create UserConstantData.cs

---
+++ UserConstantData.cs
@@ -0,0 +1,12 @@
+namespace Automationpractice
+{
+    static class UserConstantData
+    {
+        internal const string signupLink = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+        internal const string email = "lenamavricheva@coherentsolutions.com";
+        internal const string accountPassword = "2len@LEN";
+        internal const string name = "Lena";
+        internal const string lastname = "Mavricheva";
+        internal const string myWishlist = "http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist";
+    }
+}
